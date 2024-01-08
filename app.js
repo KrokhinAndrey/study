e174a2ce21da582195c7e739a036bdfad7bc7ca8
KrokhinAndrey <kroandreyhin@gmail.com>
2024-01-08 20:34:07 +0300

    8 Января 2024

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hii Sashka");
+const contribution = 11000;
+const percent = 0.07;
+const priceHome = 13500;
+const term = 24;
+
+const result = contribution * (1 + percent / 12) ** term;  
+
+if (result >= priceHome) {
+    console.log(`WIN. I have ${result}`);
+} else {
+    console.log(`LOSE. You don't have enough ${(result - priceHome)*(-1)} `);
+}
