f4480f2c0cd81b86a9171395995ae0de64ff9185
KrokhinAndrey <krokhinandreyh@gmail.com>
2024-01-13 15:11:56 +0300

    app.js mod 13.1

--- app.js
+++ app.js
@@ -35,11 +35,37 @@ if (res === 'Я не робот'){
 }
 */
 
-const balance = 1001;
-const bonusBalance = 100;
-isBanned = false;
-isExist = false;
-isSelling = true;
+// const balance = 1001;
+// const bonusBalance = 100;
+// isBanned = false;
+// isExist = false;
+// isSelling = true;
 
-const canBuy = (balance > 1000 || bonusBalance > 100) && !isBanned && !isExist && isSelling;
-console.log(`Может купить ${canBuy ? 'Да': 'Нет'}`);
+// const canBuy = (balance > 1000 || bonusBalance > 100) && !isBanned && !isExist && isSelling;
+// console.log(`Может купить ${canBuy ? 'Да': 'Нет'}`);
+
+// const age = 20;
+// const haveJob = true;
+// const money = 2000;
+// const priceMB = 2000;
+
+
+function giveCredit(age, haveJob){
+    switch(true){
+        case age > 24 && haveJob:
+            return 500;
+        case age > 24:
+            return 100;
+        default:
+            return 0;
+    }
+}
+
+function canBuy(productPrice, age, haveJob, money){
+    const creditMoney = giveCredit(age, haveJob);
+    const allMoney = creditMoney + money;
+    return allMoney >= productPrice ? true: false;
+}
+
+
+console.log(`Результат ${canBuy(2000,25,false,1700)}`);
